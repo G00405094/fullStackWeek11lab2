608491b60171c5a476ca9a2189776b97f2b70040
rossa <g00401655@atu.ie>
2024-12-09 23:26:01 +0000

    add some css for show details

--- components/cvInfo/DeleteCvForm.js
+++ components/cvInfo/DeleteCvForm.js
@@ -1,26 +1,25 @@
 import { useState } from 'react';
-import Card from '../ui/Card';
 import classes from './DeleteCvForm.module.css';
 
 function DeleteCvForm(props) {
-    const [cvId, setCvId] = useState('');
+  const [cvId, setCvId] = useState('');
   
-    function submitHandler(event) {
-      event.preventDefault();
-      props.onDeleteCV(cvId); 
-    }
-  
-    return (
-      <form onSubmit={submitHandler}>
-        <input 
-          type="text" 
-          value={cvId} 
-          onChange={(e) => setCvId(e.target.value)} 
-          placeholder="Enter CV name to delete"
-        />
-        <button type="submit">Delete CV</button>
-      </form>
-    );
+  function submitHandler(event) {
+    event.preventDefault();
+    props.onDeleteCV(cvId); 
+  }
+
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <input 
+        type="text" 
+        value={cvId} 
+        onChange={(e) => setCvId(e.target.value)} 
+        placeholder="Enter CV name to delete"
+      />
+      <button type="submit">Delete CV</button>
+    </form>
+  );
 }
 
 export default DeleteCvForm;
--- pages/store/globalContext.js
+++ pages/store/globalContext.js
@@ -28,7 +28,7 @@ export function GlobalContextProvider(props) {
         let data = await response.json();
         setGlobals((previousGlobals) => { const newGlobals = JSON.parse(JSON.stringify(previousGlobals)); newGlobals.cvs = data.cv || []; newGlobals.dataLoaded = true;  return newGlobals })
     }
-    async function deleteCV() {
+    /*async function deleteCV() {
     if (command.cmd === 'deleteCv') {
       const response = await fetch('/api/delete-cv', {
         method: 'DELETE',
@@ -45,7 +45,7 @@ export function GlobalContextProvider(props) {
         return newGlobals;
       });
     }
-}
+}*/
     
 
     async function editGlobalData(command) { // {cmd: someCommand, newVal: 'new text'}
@@ -67,12 +67,22 @@ export function GlobalContextProvider(props) {
                     'Content-Type': 'application/json'
                 }
             });
+
             const data = await response.json(); // Should check here that it worked OK
             setGlobals((previousGlobals) => {
                 const newGlobals = JSON.parse(JSON.stringify(previousGlobals))
                 newGlobals.cvs.push(command.newVal); return newGlobals
             })
             
+        }
+        if (command.cmd == 'deleteCv') {
+            const response = await fetch('/api/delete-cv', {
+                method: 'DELETE',
+                body: JSON.stringify(command.name),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
         }
         
     }
